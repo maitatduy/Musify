a33da1fa52e8fa1214c817432ed3bef7c0180ac9
maitatduy <maitatduy.work@gmail.com>
2025-09-27 20:01:47 +0700

    feat: add Search component

--- src/app/components/search/Search.tsx
+++ src/app/components/search/Search.tsx
@@ -1,9 +1,22 @@
+import { FaMagnifyingGlass } from "react-icons/fa6";
+
 export default function Search() {
-    return (
-        <>
-            <div className={"bg-[#212121] rounded-[50px] sticky top-5 left-5 mt-5 z-[999]"}>
-                <h1 className={"text-3xl text-white font-bold"}>Search</h1>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <form className="bg-[#212121] rounded-[50px] mt-[20px] sticky top-[20px] left-[20px] z-[999] py-[15px] px-[30px] flex items-center">
+        <input
+          type="text"
+          name="keyword"
+          placeholder="Tìm kiếm..."
+          className="order-2 flex-1 outline-none bg-transparent text-white text-[16px] font-[600]"
+        />
+        <button
+          type="submit"
+          className="text-white order-1 text-[22px] mr-[20px]"
+        >
+          <FaMagnifyingGlass />
+        </button>
+      </form>
+    </>
+  );
 }
