d87b256f88bc97004633813f9914090c28ae8ddd
maitatduy <maitatduy.work@gmail.com>
2025-09-28 16:01:18 +0700

    feat: add Home page

--- src/app/(pages)/(home)/page.tsx
+++ src/app/(pages)/(home)/page.tsx
@@ -1,7 +1,16 @@
+import ArtistSection from "./ArtistSection";
+import CategorySection from "./CategorySection";
+import FeaturedSection from "./FeaturedSection";
+
 export default function HomePage() {
   return (
-      <>
-          <h1 className={"text-3xl text-black font-bold bg-white h-[1000px]"}>Trang chủ</h1>
-      </>
+    <>
+      {/* Featured Playlist */}
+      <FeaturedSection />
+      {/* Categories Section */}
+      <CategorySection />
+      {/* Artist Section */}
+      <ArtistSection />
+    </>
   );
 }
