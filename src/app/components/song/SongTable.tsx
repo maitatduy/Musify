e33b54cd4f3eb1f5a8c3df573fd08518f74bec11
maitatduy <maitatduy.work@gmail.com>
2025-09-29 18:25:35 +0700

    feat: add SongTable component

---
+++ src/app/components/song/SongTable.tsx
@@ -0,0 +1,22 @@
+import { ISongItem } from "@/app/interfaces/ISongItem";
+import SongRow from "./SongRow";
+
+export default function SongTable(props: { data: ISongItem[] }) {
+  const { data } = props;
+
+  return (
+    <>
+      <div className="grid grid-cols-1 gap-[10px]">
+        {data.map((item, index) => (
+          <SongRow
+            key={index}
+            image={item.image}
+            title={item.title}
+            singer={item.singer}
+            time={item.time}
+          />
+        ))}
+      </div>
+    </>
+  );
+}
