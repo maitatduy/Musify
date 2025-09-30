6dfdb07f44eca125fb43e909d03f9db25532dc5a
maitatduy <maitatduy.work@gmail.com>
2025-09-30 22:15:08 +0700

    update: support custom className prop

--- src/app/components/title/Title.tsx
+++ src/app/components/title/Title.tsx
@@ -1,8 +1,12 @@
-export default function Title(props: { text: string }) {
-  const { text } = props;
+export default function Title(props: { text: string; className?: string }) {
+  const { text, className } = props;
   return (
     <>
-      <h2 className="font-bold text-2xl text-[#EFEEE0] capitalize mb-5">
+      <h2
+        className={
+          "font-bold text-2xl text-[#EFEEE0] capitalize mb-5 " + className
+        }
+      >
         {text}
       </h2>
     </>
