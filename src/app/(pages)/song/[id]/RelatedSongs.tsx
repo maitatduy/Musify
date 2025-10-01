bba2ccb2f56b49c5d0b1e1fb09522298240d9eec
maitatduy <maitatduy.work@gmail.com>
2025-10-01 21:02:56 +0700

    feat: add RelatedSongs with firebase songs data

--- src/app/(pages)/song/[id]/RelatedSongs.tsx
+++ src/app/(pages)/song/[id]/RelatedSongs.tsx
@@ -1,47 +1,10 @@
 import SongTable from "@/app/components/song/SongTable";
 import Title from "@/app/components/title/Title";
-import { ISongItem } from "@/app/interfaces/ISongItem";
+import { getSongsList } from "@/app/helpers/getSongs";
 
-export default function RelatedSongs() {
-  const data: ISongItem[] = [
-    {
-      image: "/demo/image-3.png",
-      title: "Cô Phòng",
-      singer: "Hồ Quang Hiếu, Huỳnh Văn",
-      time: "4:32",
-    },
-    {
-      image: "/demo/image-3.png",
-      title: "Cô Phòng",
-      singer: "Hồ Quang Hiếu, Huỳnh Văn",
-      time: "4:32",
-    },
-    {
-      image: "/demo/image-3.png",
-      title: "Cô Phòng",
-      singer: "Hồ Quang Hiếu, Huỳnh Văn",
-      time: "4:32",
-    },
-    {
-      image: "/demo/image-3.png",
-      title: "Cô Phòng",
-      singer: "Hồ Quang Hiếu, Huỳnh Văn",
-      time: "4:32",
-    },
-    {
-      image: "/demo/image-3.png",
-      title: "Cô Phòng",
-      singer: "Hồ Quang Hiếu, Huỳnh Văn",
-      time: "4:32",
-    },
-    {
-      image: "/demo/image-3.png",
-      title: "Cô Phòng",
-      singer: "Hồ Quang Hiếu, Huỳnh Văn",
-      time: "4:32",
-    },
-  ];
-
+export default async function RelatedSongs(props: { categoryId: string }) {
+  const { categoryId } = props;
+  const data: any[] = await getSongsList(undefined, categoryId);
   return (
     <>
       <Title text={"Bài Hát Cùng Danh Mục"} />
