449e10374f82ad8a360624cf7059bb18f7dd3193
maitatduy <maitatduy.work@gmail.com>
2025-10-02 00:34:55 +0700

    feat: connect SingerDetailPage to firebase get singer detail

--- src/app/(pages)/singers/[id]/SongsBySinger.tsx
+++ src/app/(pages)/singers/[id]/SongsBySinger.tsx
@@ -1,51 +1,18 @@
 import SongTable from "@/app/components/song/SongTable";
 import Title from "@/app/components/title/Title";
+import { getSongsList } from "@/app/helpers/getSongs";
 import { ISongItem } from "@/app/interfaces/ISongItem";
 
-export default function SongsBySinger() {
-  const data: ISongItem[] = [
-    {
-      image: "/demo/image-3.png",
-      title: "Cô Phòng",
-      singer: "Hồ Quang Hiếu, Huỳnh Văn",
-      time: "4:32",
-    },
-    {
-      image: "/demo/image-3.png",
-      title: "Cô Phòng",
-      singer: "Hồ Quang Hiếu, Huỳnh Văn",
-      time: "4:32",
-    },
-    {
-      image: "/demo/image-3.png",
-      title: "Cô Phòng",
-      singer: "Hồ Quang Hiếu, Huỳnh Văn",
-      time: "4:32",
-    },
-    {
-      image: "/demo/image-3.png",
-      title: "Cô Phòng",
-      singer: "Hồ Quang Hiếu, Huỳnh Văn",
-      time: "4:32",
-    },
-    {
-      image: "/demo/image-3.png",
-      title: "Cô Phòng",
-      singer: "Hồ Quang Hiếu, Huỳnh Văn",
-      time: "4:32",
-    },
-    {
-      image: "/demo/image-3.png",
-      title: "Cô Phòng",
-      singer: "Hồ Quang Hiếu, Huỳnh Văn",
-      time: "4:32",
-    },
-  ];
-
+export default async function SongsBySinger(props: { singerId: string }) {
+  const { singerId } = props;
+  const data: ISongItem[] = await getSongsList();
+  const dataFilter = data.filter((item: any) =>
+    item.singerId.includes(singerId)
+  );
   return (
     <>
       <Title text={"Danh sách bài hát"} />
-      <SongTable data={data} />
+      <SongTable data={dataFilter} />
     </>
   );
 }
--- src/app/(pages)/singers/[id]/page.tsx
+++ src/app/(pages)/singers/[id]/page.tsx
@@ -1,17 +1,21 @@
 import CardInfo from "@/app/components/card/CardInfo";
 import SongsBySinger from "./SongsBySinger";
+import { getSingerDetail } from "@/app/helpers/getSingers";
 
-export default function SingerDetailPage() {
+export default async function SingerDetailPage({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const data: any = await getSingerDetail(params.id);
   return (
     <>
       <CardInfo
-        image={"/demo/image-11.png"}
-        title={"Sơn Tùng M-TP"}
-        description={
-          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
-        }
+        image={data.image}
+        title={data.title}
+        description={data.description}
       />
-      <SongsBySinger />
+      <SongsBySinger singerId={data.id} />
     </>
   );
 }
