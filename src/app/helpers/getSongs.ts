503c18f4f81a20c947c74a9a3364345d16bba920
maitatduy <maitatduy.work@gmail.com>
2025-10-01 21:00:35 +0700

    feat: add getSongsList and getSongsDetail for songs data

---
+++ src/app/helpers/getSongs.ts
@@ -0,0 +1,52 @@
+import { ref, onValue, query, orderByChild, equalTo } from "firebase/database";
+import { database } from "../firebaseConfig";
+
+export const getSongsList = async (max?: number, categoryId?: string) => {
+  const songRef = ref(database, "songs");
+  let songQuery: any;
+  if (categoryId) {
+    songQuery = query(songRef, orderByChild("categoryId"), equalTo(categoryId));
+  } else {
+    songQuery = ref(database, "songs");
+  }
+
+  const result: any[] = await new Promise((resolve) => {
+    onValue(songQuery, (snapshot) => {
+      let data: any = [];
+      snapshot.forEach((childSnapshot) => {
+        const childKey = childSnapshot.key;
+        const childValue = childSnapshot.val();
+        data.push({
+          id: childKey,
+          ...childValue,
+        });
+      });
+      if (max) {
+        data = data.slice(0, max);
+      }
+      resolve(data);
+    });
+  });
+  return result;
+};
+
+export const getSongsDetail = async (id: string) => {
+  const songRef = ref(database, "songs/" + id);
+
+  const result: any[] = await new Promise((resolve: any) => {
+    onValue(songRef, (snapshot) => {
+      const childKey = snapshot.key;
+      const childValue = snapshot.val();
+      if (childValue) {
+        const data = {
+          id: childKey,
+          ...childValue,
+        };
+        resolve(data);
+      } else {
+        resolve(null);
+      }
+    });
+  });
+  return result;
+};
