9fd90d83b44fee0163f31a00491d6abf37fb4eb2
maitatduy <maitatduy.work@gmail.com>
2025-10-01 13:53:39 +0700

    feat: fetch song list from database

--- src/app/(pages)/categories/page.tsx
+++ src/app/(pages)/categories/page.tsx
@@ -1,80 +1,10 @@
 import CardList from "@/app/components/card/CardList";
 import Title from "@/app/components/title/Title";
+import { getDataCategories } from "@/app/helpers/getCategories";
 import { ICardItem } from "@/app/interfaces/ICardItem";
 
-export default function CategoriesPage() {
-  const dataCategories: ICardItem[] = [
-    {
-      image: "/demo/image-6.png",
-      title: "Nhạc Trẻ",
-      description:
-        "Top 100 Nhạc Trẻ là danh sách 100 ca khúc hot nhất hiện tại của thể loại Nhạc Trẻ",
-      link: "",
-    },
-    {
-      image: "/demo/image-7.png",
-      title: "Pop Âu Mỹ",
-      description:
-        "Top 100 Nhạc Pop Âu Mỹ là danh sách 100 ca khúc hot nhất hiện tại của thể loại Top 100 Nhạc Pop Âu Mỹ",
-      link: "",
-    },
-    {
-      image: "/demo/image-8.png",
-      title: "Nhạc EDM",
-      description:
-        "Top 100 Nhạc Electronic/Dance Âu Mỹ là danh sách 100 ca khúc hot nhất hiện tại của thể loại Top 100 Nhạc Electronic/Dance Âu Mỹ",
-      link: "",
-    },
-    {
-      image: "/demo/image-9.png",
-      title: "Nhạc Trữ Tình",
-      description:
-        "Top 100 Nhạc Trẻ là danh sách 100 ca khúc hot nhất hiện tại của thể loại Nhạc Trẻ",
-      link: "",
-    },
-    {
-      image: "/demo/image-10.png",
-      title: "Nhạc Hàn Quốc",
-      description:
-        "Top 100 Nhạc Trẻ là danh sách 100 ca khúc hot nhất hiện tại của thể loại Nhạc Trẻ",
-      link: "",
-    },
-    {
-      image: "/demo/image-6.png",
-      title: "Nhạc Trẻ",
-      description:
-        "Top 100 Nhạc Trẻ là danh sách 100 ca khúc hot nhất hiện tại của thể loại Nhạc Trẻ",
-      link: "",
-    },
-    {
-      image: "/demo/image-7.png",
-      title: "Pop Âu Mỹ",
-      description:
-        "Top 100 Nhạc Pop Âu Mỹ là danh sách 100 ca khúc hot nhất hiện tại của thể loại Top 100 Nhạc Pop Âu Mỹ",
-      link: "",
-    },
-    {
-      image: "/demo/image-8.png",
-      title: "Nhạc EDM",
-      description:
-        "Top 100 Nhạc Electronic/Dance Âu Mỹ là danh sách 100 ca khúc hot nhất hiện tại của thể loại Top 100 Nhạc Electronic/Dance Âu Mỹ",
-      link: "",
-    },
-    {
-      image: "/demo/image-9.png",
-      title: "Nhạc Trữ Tình",
-      description:
-        "Top 100 Nhạc Trẻ là danh sách 100 ca khúc hot nhất hiện tại của thể loại Nhạc Trẻ",
-      link: "",
-    },
-    {
-      image: "/demo/image-10.png",
-      title: "Nhạc Hàn Quốc",
-      description:
-        "Top 100 Nhạc Trẻ là danh sách 100 ca khúc hot nhất hiện tại của thể loại Nhạc Trẻ",
-      link: "",
-    },
-  ];
+export default async function CategoriesPage() {
+  const dataCategories: ICardItem[] = await getDataCategories(5);
 
   return (
     <>
