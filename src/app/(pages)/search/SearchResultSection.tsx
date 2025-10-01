7885b0de5b5e9bc2f2ca8b9c9b5e6ad8e49b3c54
maitatduy <maitatduy.work@gmail.com>
2025-10-01 21:59:46 +0700

    feat: fetch songs from Firebase and filter by keyword

--- src/app/(pages)/search/SearchResultSection.tsx
+++ src/app/(pages)/search/SearchResultSection.tsx
@@ -1,46 +1,26 @@
+"use client";
+
 import SongTable from "@/app/components/song/SongTable";
 import Title from "@/app/components/title/Title";
-import { ISongItem } from "@/app/interfaces/ISongItem";
+import { getSongsList } from "@/app/helpers/getSongs";
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function SearchResultSection() {
-  const data: ISongItem[] = [
-    {
-      image: "/demo/image-3.png",
-      title: "Cô Phòng",
-      singer: "Hồ Quang Hiếu, Huỳnh Văn",
-      time: "4:32",
-    },
-    {
-      image: "/demo/image-3.png",
-      title: "Cô Phòng",
-      singer: "Hồ Quang Hiếu, Huỳnh Văn",
-      time: "4:32",
-    },
-    {
-      image: "/demo/image-3.png",
-      title: "Cô Phòng",
-      singer: "Hồ Quang Hiếu, Huỳnh Văn",
-      time: "4:32",
-    },
-    {
-      image: "/demo/image-3.png",
-      title: "Cô Phòng",
-      singer: "Hồ Quang Hiếu, Huỳnh Văn",
-      time: "4:32",
-    },
-    {
-      image: "/demo/image-3.png",
-      title: "Cô Phòng",
-      singer: "Hồ Quang Hiếu, Huỳnh Văn",
-      time: "4:32",
-    },
-    {
-      image: "/demo/image-3.png",
-      title: "Cô Phòng",
-      singer: "Hồ Quang Hiếu, Huỳnh Văn",
-      time: "4:32",
-    },
-  ];
+  const searchParams = useSearchParams();
+  const keywordDefault = searchParams.get("keyword") || "";
+  const [data, setData] = useState<any[]>([]);
+
+  useEffect(() => {
+    const fetchAPI = async () => {
+      const data: any = await getSongsList();
+      const regex = new RegExp(keywordDefault, "i");
+      const dataFilter = data.filter((item: any) => regex.test(item.title));
+      setData(dataFilter);
+    };
+
+    fetchAPI();
+  }, [keywordDefault]);
 
   return (
     <>
