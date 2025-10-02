c5a27428b8aebdf46a3f2ba017051756ba6fc197
maitatduy <maitatduy.work@gmail.com>
2025-10-02 14:54:56 +0700

    feat: display singers name in SongDetailPage

--- src/app/(pages)/song/[id]/page.tsx
+++ src/app/(pages)/song/[id]/page.tsx
@@ -3,6 +3,7 @@ import LyricsBox from "./LyricsBox";
 import RelatedSongs from "./RelatedSongs";
 import { getSongsDetail } from "@/app/helpers/getSongs";
 import { notFound } from "next/navigation";
+import { getSingers } from "@/app/helpers/getSingers";
 
 export default async function SongDetailPage({
   params,
@@ -13,10 +14,21 @@ export default async function SongDetailPage({
   if (!data) {
     notFound();
   }
+  const songDetail: any = await getSongsDetail(params.id);
+  let singersList: any[] = await getSingers();
+  const singersId: any[] = songDetail.singerId;
+  const filteredSingers = singersList.filter((item) =>
+    singersId.includes(item.id)
+  );
+  const singerName = filteredSingers.map((item) => item.title).join(", ");
   return (
     <>
       {/* CardInfo */}
-      <CardInfo image={data.image} title={data.title} description={""} />
+      <CardInfo
+        image={data.image}
+        title={data.title}
+        description={singerName}
+      />
       {/* Lời bài hát */}
       <LyricsBox lyric={data.lyric} />
 
