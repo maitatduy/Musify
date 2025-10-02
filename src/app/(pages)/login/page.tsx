94cc434a49cf71d517061b3913bff1920c1a6181
maitatduy <maitatduy.work@gmail.com>
2025-10-02 22:31:31 +0700

    feat: add login functionality

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -1,13 +1,39 @@
+"use client";
+
 import FormButton from "@/app/components/form/FormButton";
 import FormInput from "@/app/components/form/FormInput";
 import Title from "@/app/components/title/Title";
+import { authFirebase } from "@/app/firebaseConfig";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useRouter } from "next/navigation";
 
 export default function LoginPage() {
+  const router = useRouter();
+  const handleLogin = (event: any) => {
+    event.preventDefault();
+    const email = event.target.email.value.trim();
+    const password = event.target.password.value.trim();
+
+    if (!email || !password) {
+      alert("Vui lòng nhập đầy đủ thông tin");
+      return;
+    }
+    signInWithEmailAndPassword(authFirebase, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        if (user) {
+          router.push("/");
+        }
+      })
+      .catch((error) => {
+        alert("Tài khoản hoặc mật khẩu không chính xác!");
+      });
+  };
   return (
     <>
       <div className="mt-[60px] w-[500px] mx-auto">
         <Title text="Đăng Nhập Tài Khoản" className="text-center" />
-        <form className="">
+        <form className="" method="post" onSubmit={handleLogin}>
           <FormInput
             label="Email"
             type="email"
